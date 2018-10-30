2b27c2b2b667b477faf461eb436079f2bda3796d
muhdazmitomirin <muhdazmitomirin@gmail.com>
2018-10-30 17:07:47 +0800

    CRUD Aktiviti

--- penganjur/urls.py
+++ penganjur/urls.py
@@ -6,4 +6,10 @@ urlpatterns = [
 
 	# http://127.0.0.1:8000/penganjur/aktiviti/add
 	path('aktiviti/add/', views.addaktiviti , name='add_aktiviti'),
+
+	# http://127.0.0.1:8000/penganjur/aktiviti/add
+	path('aktiviti/<int:pk>/delete', views.delete_aktiviti , name='delete_aktiviti'),
+
+	# http://127.0.0.1:8000/penganjur/aktiviti/add
+	path('aktiviti/<int:pk>/edit', views.editaktiviti , name='edit_aktiviti'),
 ]
--- penganjur/views.py
+++ penganjur/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Aktiviti
 from .forms import AktivitiForm
+from django.urls import reverse_lazy
 
 
 # Create your views here.
@@ -9,12 +10,12 @@ from .forms import AktivitiForm
 def home(request):
 
 	#aktivitiid = request.GET['aktivitiid']
-	print(request.GET['aktivitid'])
+	#print(request.GET['aktivitid'])
 	a = Aktiviti.objects.all()
-	for ak in a:
-		print(ak.tajuk,ak.tempat,ak.penceramah)
+	# for ak in a:
+	# 	print(ak.tajuk,ak.tempat,ak.penceramah)
 
-	return render(request,'penganjur/home.html')
+	return render(request,'penganjur/home.html',{'aktiviti' : a})
 
 # update aktiviti
 def update_aktiviti(request,pk):
@@ -55,8 +56,49 @@ def delete_penganjur(request,pk):
 # Tambah aktiviti
 def addaktiviti(request):
 
+	# user dah submit form / POST
 	if request.method == "POST":
-		pass
+		form = AktivitiForm(request.POST)
+
+		if form.is_valid():
+			#simpan dalam memori
+			aktiviti = form.save(commit=False)
+			#update database
+			aktiviti.save()
+			return redirect(reverse_lazy('home_penganjur'))
 	else:
 		form = AktivitiForm()
+		print(form)
+
 	return render(request,'penganjur/tambahaktiviti.html', {'form': form})
+
+	# hapus aktiviti guna form
+def delete_aktiviti(request,pk):
+
+	aktiviti = get_object_or_404(Aktiviti,pk=pk)
+
+	if request.method == "POST":
+
+		if request.POST.get("submit_yes"):
+
+			aktiviti.delete()
+			return redirect(reverse_lazy('home_penganjur'))
+
+	return render(request,
+		'penganjur/confirm_delete_aktiviti.html',
+		{'aktiviti' : aktiviti })
+
+def editaktiviti(request,pk):
+
+	aktiviti = get_object_or_404(Aktiviti,pk=pk)
+	if request.method == "POST":
+		form = AktivitiForm(request.POST,instance=aktiviti)
+
+		if form.is_valid():
+			aktiviti = form.save(commit=False)
+			aktiviti.save()
+			return redirect(reverse_lazy('home_penganjur'))
+	else:
+		form = AktivitiForm(instance=aktiviti)
+
+	return render(request,'penganjur/editaktiviti.html', {'form': form})
